259a6fe8859c6b146666a92622baa70047148c69
Jabiyeff <cebiyevanar@gmail.com>
2020-09-08 11:37:18 +0400

    santoni: prebuilts: add prebuilts folder, move Youtube => prebuilts/Youtube, add GalleryGo, CameraGo and remove some packages.
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

---
+++ prebuilts/Android.mk
@@ -0,0 +1,59 @@
+#
+# Copyright 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Youtube
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := Youtube/Youtube.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := CameraGo
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := CameraGo/CameraGo.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
+LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Snap Camera2 Camera
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GalleryGo
+LOCAL_OVERRIDES_PACKAGES := Gallery2 Photos QPGallery
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := GalleryGo/GalleryGo.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback ViaBrowser Browser Browser2 DuckDuckGo Eleven TrebuchetQuickStep Email Exchange2
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
