50932e258c695538f559a8c35e243895bb4a975a
Jabiyeff <cebiyevanar@gmail.com>
2020-09-10 12:53:43 +0400

    santoni: remove some system apps
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- prebuilts/Android.mk
+++ prebuilts/Android.mk
@@ -63,7 +63,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback ViaBrowser Browser Browser2 DuckDuckGo Eleven TrebuchetQuickStep Email Exchange2
+LOCAL_OVERRIDES_PACKAGES := AudioFX Jelly GoogleTTS talkback ViaBrowser Browser Browser2 DuckDuckGo Eleven TrebuchetQuickStep Email Exchange2 FMRadio Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
