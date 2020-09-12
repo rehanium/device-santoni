743bb0405b5af8db13394381562ec09d5a347e16
Vinay Verma <vvinay@codeaurora.org>
2020-09-12 22:26:36 +0400

    santoni: Enable PRODUCT_MINIMIZE_JAVA_DEBUG_INFO Flag is enabled to reduce system image size by limiting java debug info.
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -506,3 +506,6 @@ PRODUCT_PACKAGES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+# Reduce system image size by limiting java debug info.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
