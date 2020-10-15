304ca79fa06a4462dcde45dc4730d3477977e926
Jabiyeff <cebiyevanar@gmail.com>
2020-10-15 18:06:48 +0400

    Revert "santoni: Remove deprecated android.hardware.thermal@1.0 HAL"
    This reverts commit 22ad64dd4b2049e64b2ee56c22c95c23fede5e81.

--- device.mk
+++ device.mk
@@ -456,6 +456,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
     thermal.msm8937
 
 PRODUCT_COPY_FILES += \
