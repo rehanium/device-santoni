e9c1df664be8d4ad90c26fba2f2736926b483025
Jabiyeff <cebiyevanar@gmail.com>
2020-10-15 21:29:56 +0400

    Revert "santoni: Remove telephony-ext completely"
    This reverts commit 4c83da9865643606f644c45e122d475c5f1b51cc.

--- device.mk
+++ device.mk
@@ -419,10 +419,14 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    telephony-ext \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
-    qti_telephony_utils.xml
+    qti_telephony_utils.xml \
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
