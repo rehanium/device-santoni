c03037b0f3eeb3cebbc39f1bd9a80f9f3fa47ea4
Muhammed Siju <msiju@codeaurora.org>
2020-10-16 14:39:33 +0400

    santoni: : Disable advance network scan for legacy targets - Set persist.vendor.radio.enableadvancedscan to false
    Change-Id: Id01e7a545e89ff9ab9fb659aec1fbae527c40f71
    CRs-Fixed: 2780132

--- device.mk
+++ device.mk
@@ -406,6 +406,10 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
+#vendor prop to disable advanced network scanning
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.enableadvancedscan=false
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext \
