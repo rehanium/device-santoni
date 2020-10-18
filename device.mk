1451d4c787cb6ad464b87690e0d2f0377f0cdbbf
Jabiyeff <cebiyevanar@gmail.com>
2020-10-18 17:51:06 +0400

    santoni: Update audio.common util from 6.0

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0 \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.common@5.0 \
-    android.hardware.audio.common@5.0-util \
+    android.hardware.audio.common@6.0-util \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@6.0-impl \
