a67a872bcc7fb065264f86dd63fcc541da0ceaa2
Jabiyeff <cebiyevanar@gmail.com>
2020-10-20 23:17:26 +0400

    santoni: Use default light service

--- device.mk
+++ device.mk
@@ -306,7 +306,11 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_santoni
+    lights.msm8937
+
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Media
 PRODUCT_COPY_FILES += \
