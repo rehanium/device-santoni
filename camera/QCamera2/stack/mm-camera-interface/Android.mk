e70a8cb36d2b963da492085b0fbaeb29dc74437a
Jabiyeff <cebiyevanar@gmail.com>
2020-10-27 22:17:27 +0400

    santoni: camera: Update camera_common_headers location
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -6,7 +6,6 @@ include $(CLEAR_VARS)
 
 LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_HEADER_LIBRARIES += media_plugin_headers
-LOCAL_HEADER_LIBRARIES += camera_common_headers
 
 MM_CAM_FILES := \
         src/mm_camera_interface.c \
@@ -58,6 +57,7 @@ LOCAL_SRC_FILES := $(MM_CAM_FILES)
 
 LOCAL_MODULE           := libmmcamera_interface
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog
+LOCAL_HEADER_LIBRARIES := camera_common_headers
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 
